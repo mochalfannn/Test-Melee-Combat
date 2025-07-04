bc4bad1e62eb899f62fb9f5668bf1c8b6f433fcc
moichi <124658975+mochalfannn@users.noreply.github.com>
2025-07-04 19:32:06 +0700

    upadate button quit

---
+++ Assets/MenuController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnExitClick() {
+
+        Application.Quit();
+    
+    }
+    
+}
